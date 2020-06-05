f18d79e861016a5a570460021df45844857b89c8
hongyiwx <hongyix.wang@intel.com>
2020-06-05 14:28:39 +0800

    添加一些注释

--- ModTest/ModTest.cs
+++ ModTest/ModTest.cs
@@ -8,6 +8,8 @@ namespace ModTest
     public class Main:MBSubModuleBase
     {
         protected override void OnSubModuleLoad(){
+            base.OnSubModuleLoad();
+            /*在主菜单中添加一个选项，命名为Message，点击后在左下角显示信息“Hello World!”*/
             Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
                 new TextObject("Message", null),
                 9990,

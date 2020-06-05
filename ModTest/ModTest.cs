529c1960d2be8170172e33515bb95d77c8ea5b85
hongyiwx <hongyix.wang@intel.com>
2020-06-05 14:08:59 +0800

    添加项目文件。

---
+++ ModTest/ModTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace ModTest
+{
+    public class Main:MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad(){
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                new TextObject("Message", null),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                false));
+        }
+    }
+}
